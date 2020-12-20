47637ce61fdd91ad9cb7fe6ef04b8b8772247333
newaeonweb <fsilvasmonteiro@gmail.com>
2020-12-20 15:59:24 +0000

    Deploying to gh-pages from  @ 46512c3f57d017c040f97d97234a29efdbb7859c 🚀

--- ui-elements/lib/ui-elements.module.d.ts
+++
@@ -1,8 +0,0 @@
-import * as ɵngcc0 from '@angular/core';
-import * as ɵngcc1 from './navbar/navbar.component';
-export declare class UiElementsModule {
-    static ɵmod: ɵngcc0.ɵɵNgModuleDefWithMeta<UiElementsModule, [typeof ɵngcc1.NavbarComponent], never, [typeof ɵngcc1.NavbarComponent]>;
-    static ɵinj: ɵngcc0.ɵɵInjectorDef<UiElementsModule>;
-}
-
-//# sourceMappingURL=ui-elements.module.d.ts.map
